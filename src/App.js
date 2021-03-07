6472f9e975dcdd20c58b2ff2ec964ab773d34c14
AndrewQuintana22291 <aquintana22291@columbushs.com>
2021-03-07 12:12:28 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
 import logo from './logo.svg';
-import './App.css';
-import countries from "./countries.json"; 
-function App() {
-  return (
-    <div className="App">
-      Hi
-      {
-        countries.map((country) => {
-          return <h3>{country.name.common}</h3>
-        })
+ import './App.css';
+ import React from 'react';
+ import NavBar from './components/NavBar';
+ import CountriesList from './components/CountriesList';
+ import CountryDetails from './components/CountryDetails';
+ import { BrowserRouter, Route } from 'react-router-dom';
+ import countries from './countries.json';
 
-      }
-    </div>
-  );
-}
+ class App extends React.Component {
 
-export default App;
+   state = {
+     countries: countries,
+   }
+
+   render() {
+     return (
+       <div className="App">
+         <BrowserRouter>
+           <NavBar />
+           <div className="container">
+             <Route exact path="/" component={CountriesList} />
+             <Route path="/countries/:countryCCA3" component={CountryDetails} />
+           </div>
+         </BrowserRouter>
+       </div>
+     );
+   }
+
+ function App() {
+   return (
+     <div className="App">
+       <header className="App-header">
+         <img src={logo} className="App-logo" alt="logo" />
+         <p>
+           Edit <code>src/App.js</code> and save to reload.
+         </p>
+         <a
+           className="App-link"
+           href="https://reactjs.org"
+           target="_blank"
+           rel="noopener noreferrer"
+         >
+           Learn React
+         </a>
+       </header>
+     </div>
+   );
+  }
+
+ export default App;
